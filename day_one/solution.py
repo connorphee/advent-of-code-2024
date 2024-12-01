531fb28f7357ea4dde9988b88f590c2f7d95cb15
Connor Phee <connor.phee@manscaped.com>
2024-12-01 17:00:45 -0500

    chore: update folder naming convention to use word instead of number

--- day_one/solution.py
+++ day_one/solution.py
@@ -3,7 +3,7 @@ from typing import List
 
 
 class DayOneSolution:
-    def find_difference(self, list_one: List[int], list_two: List[int]) -> int:
+    def part_one(self, list_one: List[int], list_two: List[int]) -> int:
         sorted_one = sorted(list_one)
         sorted_two = sorted(list_two)
         return sum(abs(a - b) for a,b in zip(sorted_one, sorted_two))
@@ -11,23 +11,26 @@ class DayOneSolution:
 
 class DayOneUnitTest(unittest.TestCase):
 
-    def test_example(self):
+    def setUp(self):
+        self.solution = DayOneSolution()
+
+    def test_part_one_example(self):
         list_one = [3, 4, 2, 1, 3, 3]
         list_two = [4, 3, 5, 3, 9, 3]
 
-        self.assertEqual(11, DayOneSolution().find_difference(list_one, list_two))
+        self.assertEqual(11, self.solution.part_one(list_one, list_two))
 
-    def test_input(self):
+    def test_part_one_input(self):
         list_one = []
         list_two = []
-        with open('day1/input.txt') as file:
+        with open('./day_one/input.txt') as file:
             for line in file:
                 split = line.strip().split("   ")
                 if(len(split) == 2): 
                     list_one.append(int(split[0]))
                     list_two.append(int(split[1]))
 
-        self.assertEqual(2031679, DayOneSolution().find_difference(list_one, list_two))
+        self.assertEqual(2031679, self.solution.part_one(list_one, list_two))
 
         
         
